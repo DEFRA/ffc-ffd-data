36d2dd68cabea65cc6707d4d81bfd5946019ece7
Rana Salem <ranatasalem@gmail.com>
2024-01-29 16:31:45 +0000

    Refactoring GraphQL Schema (#2)

--- app/graphql/server.js
+++ app/graphql/server.js
@@ -1,37 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
-const { get } = require('../data')
-
-const typeDefs = `
-  type Query {
-    customerBusinesses: CustomerBusinesses
-  }
-
-  type Business {
-    id: Int
-    sbi: String
-    name: String
-  }
-
-  type CustomerBusinesses {
-    crn: String
-    businesses: [Business]
-  }
-`
-const resolvers = {
-  Query: {
-    customerBusinesses: async (_root, _args, context) => {
-      const response = await get('/organisation/person/3337243/summary?search=', context.crn, context.token)
-      return {
-        crn: context.crn,
-        businesses: response._data?.map(business => ({
-          id: business.id,
-          sbi: business.sbi,
-          name: business.name
-        })) ?? []
-      }
-    }
-  }
-}
+const { typeDefs } = require('./schema/typeDefs')
+const { resolvers } = require('./schema/resolvers')
 
 const apolloServer = new ApolloServer({
   typeDefs,
