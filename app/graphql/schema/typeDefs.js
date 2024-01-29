b2af3fef1f79a9926e28d81663baa282ce51f460
Rana Salem <ranatasalem@gmail.com>
2024-01-29 10:40:47 +0000

    fix typo in typeDefs

--- app/graphql/schema/typeDefs.js
+++ app/graphql/schema/typeDefs.js
@@ -11,12 +11,8 @@ type Business {
 
 type CustomerBusinesses {
   crn: String!
-  business: [Business!]!
+  businesses: [Business!]!
 }
 `
-
-// on line 7, ID is one of the scalar types in GraphQL, maybe this would be more appropriate?
-// if the query doesn't work via Postman, might be worth changing this back to Int
-// exclamation points are added at the end to make the values not null
 
 module.exports = { typeDefs }
